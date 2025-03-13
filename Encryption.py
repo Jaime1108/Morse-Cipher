26444c5007d32cb43192a41ccf7c0db6895e32ed
James Nguyen <dangkhoan1108@gmail.com>
2025-03-13 00:51:34 -0500

    Update Encryption.py

--- Encryption.py
+++ Encryption.py
@@ -3,15 +3,21 @@ from Crypto.Random import get_random_bytes
 from Crypto.Util.Padding import pad, unpad
 import base64
 
+key = get_random_bytes(16)
+IV = get_random_bytes(AES.block_size)
 def Encryption(text):
-    key = get_random_bytes(16)
-    IV = get_random_bytes(AES.block_size)
+    
     cipher = AES.new(key, AES.MODE_CBC, IV)
     ciphertext = cipher.encrypt(pad(text,AES.block_size))
     return base64.b64encode(ciphertext).decode()
 #ciphertext
 
-text = b'cyka-blyat asd asdafdsa casdfasdfac casdcasd'
-print("original: ",text)
+original = "cyka-blyat asd asdafdsa casdfasdfac casdcasd"
+text= original.encode()
+print("original: ",original)
 encrypted = Encryption(text)
 print(encrypted)
+decodedKey = base64.b64encode(key).decode()
+decodedIV = base64.b64encode(IV).decode()
+print(decodedKey)
+print(decodedIV)
